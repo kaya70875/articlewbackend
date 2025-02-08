f928830725a004ba0a1f1a5728cbf2368ccbcfd9
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-08 18:43:46 +0300

    disable nltk for testing railway deploy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.routes.sentences import router as sentences_route
 from app.routes.ai import router as ai_route
-from app.routes.wordInfo import router as wordInfo_route
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -16,7 +15,7 @@ app.add_middleware(
 # Register your routes
 app.include_router(sentences_route, prefix="/api", tags=["Sentences"])
 app.include_router(ai_route, prefix="/api", tags=["AI"])
-app.include_router(wordInfo_route, prefix="/api", tags=["WordInfo"])
+#app.include_router(wordInfo_route, prefix="/api", tags=["WordInfo"])
 
 # Run the app
 if __name__ == "__main__":
