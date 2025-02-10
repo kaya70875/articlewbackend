a120d143499dbbfa8ff22918e827305e14e6137f
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-10 20:52:12 +0300

    add word similarity endpoint and similarity score calculation

--- app/routes/wordInfo.py
+++ app/routes/wordInfo.py
@@ -3,6 +3,7 @@ from app.models.word_info import WordInfoResponse
 from word.wordNet import get_word_info_extended
 from word.spacyWord import get_word_pos
 import asyncio
+from word.spacyWord import calculate_similarity_score
 
 router = APIRouter()
 
@@ -22,3 +23,10 @@ async def get_word_Pos(word: str, sentence: str):
     # Call the function to get word information
     word_info = await asyncio.to_thread(get_word_pos, word, sentence)
     return {"response" : word_info}
+
+@router.get('/wordSimilarity/{word1}/{word2}')
+async def get_word_similarity(word1: str, word2: str):
+    # Call the function to get word information
+    score = await asyncio.to_thread(calculate_similarity_score, word1, word2)
+    percentage = (score * 100)
+    return {"score" : percentage.__round__(2)}
--- word/spacyWord.py
+++ word/spacyWord.py
@@ -20,3 +20,18 @@ def get_word_pos(word: str, sentence:str) -> str:
     for token in doc:
         if token.text == word:
             return token.pos_
+
+def calculate_similarity_score(word1 : str, word2 : str) -> float:
+    """
+        Calculate the similarity between two words.
+        :param word1: The first word
+        :param word2: The second word
+        :return: The similarity between the two words
+    """
+
+    word1 = nlp(word1)
+    word2 = nlp(word2)
+
+    similarity_score = word1.similarity(word2)
+
+    return similarity_score
