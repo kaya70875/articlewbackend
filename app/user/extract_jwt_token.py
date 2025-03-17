54f9475cab1d38bec198b12e4ead9b869f500050
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-17 17:03:34 +0300

    use custom package for decoding nextauth cookies

--- app/user/extract_jwt_token.py
+++ app/user/extract_jwt_token.py
@@ -1,23 +1,25 @@
-from fastapi.security import HTTPBearer
-from fastapi import Depends, Header
-from typing import Annotated
-from typing import Literal
 from app.user.utils.helpers import *
+from fastapi_nextauth_jwt import NextAuthJWTv4
+from typing import Annotated
+from fastapi import Depends, HTTPException
+import os
 
-security = HTTPBearer(
-    description="Enter your JWT token from Next Auth"
-)
+SECRET_KEY = os.getenv('JWT_SECRET')
+JWT = NextAuthJWTv4(secret=SECRET_KEY)
 
-async def get_user_id(credentials: Annotated[str, Depends(security)], provider : Literal['google', 'credentials'] = Header(None, alias='X-Provider')):
+async def get_user_id(jwt : Annotated[dict, Depends(JWT)]):
     """
     Gets user's details with decoding jwt token.
     """
-    # Get token from credentials
-    token = credentials.credentials
-        
+
+    provider = jwt['provider']
+    
     if provider == 'credentials':
-        user_id = await extract_id_from_jwt(token)
+        user_id = jwt.get('id')
+        if not user_id:
+            raise HTTPException(status_code=401, detail='User id cannot found in token.')
         return user_id
     else:
-        user_id = await extract_id_from_email(token)
+        email = jwt.get('email')
+        user_id = await extract_id_from_email(email)
         return user_id
