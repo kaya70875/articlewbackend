5722e1667add25674ca9431bc7d16d5b92e68aad
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-21 13:12:35 +0300

    create paddle webhook and other endpoints for getting prices with Python Paddle SDK

---
+++ app/routes/paddle/server.py
@@ -0,0 +1,105 @@
+from paddle_billing import Client, Environment, Options
+from typing import List
+from os import getenv
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
+from app.utils.signature import verify_paddle_signature
+from app.routes.paddle.events import *
+
+class PaddlePrice(BaseModel):
+    price_id: str
+    name: str
+    description: str
+    amount: str
+    currency: str
+    product_id: str
+
+if not getenv("PADDLE_API_SECRET"):
+    raise ValueError("PADDLE_API_SECRET is not set in the environment.")
+
+paddle = Client(getenv('PADDLE_API_SECRET'), options=Options(Environment.SANDBOX))
+
+router = APIRouter()
+
+@router.get("/paddle/prices", response_model=List[PaddlePrice])
+async def get_prices():
+    """
+    Get the prices from Paddle.
+    """
+    try:
+        prices = paddle.prices.list()
+
+        return [
+            {
+                "name" : price.name,
+                "description" : price.description,
+                "price_id": price.id,
+                "amount": price.unit_price.amount,
+                "currency": price.unit_price.currency_code.value,
+                "product_id": price.product_id
+            }
+            for price in prices
+        ]
+    except Exception as e:
+        print(f"Error retrieving prices: {e}")
+        raise HTTPException(status_code=500, detail=f"Error retrieving prices: {e}")
+
+@router.get("/paddle/subscriptions/{subscription_id}")
+async def get_subscription(subscription_id: str):
+    """
+    Get the subscription details from Paddle.
+    """
+    try:
+        print(subscription_id)
+        subscription = paddle.subscriptions.get(subscription_id)
+
+        return {
+            "subscription_id": subscription.id,
+            "next_billed_at": subscription.next_billed_at,
+            "update_url": subscription.management_urls.update_payment_method,
+            "cancel_url" : subscription.management_urls.cancel
+        }
+    except Exception as e:
+        print(f"Error retrieving subscription: {e}")
+        raise HTTPException(status_code=500, detail=f"Error retrieving subscription: {e}")
+
+@router.post("/webhook")
+async def webhook(req: Request):
+    try:
+        # Verify the signature
+        body = await req.body()
+
+        if not verify_paddle_signature(req.headers, body):
+            raise HTTPException(status_code=403, detail="Invalid signature")
+        
+        # Parse the JSON body
+        json_data = await req.json()
+        data = json_data.get("data")
+        
+        # Check if the event type is present
+        event_type = json_data.get("event_type")
+        if not event_type:
+            return HTTPException(status_code=400, detail="Missing event_type in request body")
+        
+        # Handle different event types
+        if event_type == "subscription.created":
+            # Handle subscription created event
+            await handleSubscriptionCreated(data)
+        
+        elif event_type == "subscription.updated":
+            # Handle subscription updated event
+            print('A subscription updated', data)
+        
+        elif event_type == "subscription.cancelled":
+            # Handle subscription cancelled event
+            await handleSubscriptionCanceled(data)
+    
+        else:
+            print(f"Unhandled event type: {event_type}")
+        
+        
+        return {"status": "success"}
+        
+    except Exception as e:
+        print(f"❌ Error handling webhook: {e}")
+        raise HTTPException(status_code=500, detail=f"Error handling webhook: {e}")
