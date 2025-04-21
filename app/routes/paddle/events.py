c13ef809d2412393411d7e1e36d7d819a7bce737
Ahmet Kaya <shortsforturkey@gmail.com>
2025-04-21 13:12:46 +0300

    add event handlers for subscription creation and cancellation in Paddle

---
+++ app/routes/paddle/events.py
@@ -0,0 +1,59 @@
+from app.models.database import db
+from fastapi import HTTPException
+from pymongo import ReturnDocument
+from app.routes.paddle.utils import *
+
+async def handleSubscriptionCreated(data: dict):
+    """
+    Handle the subscription created event from Paddle.
+    This function should be called when a subscription is created.
+
+    It should update the user's subscription status in the database.
+    """
+
+    try:
+        # Get relevant fields
+        sub_id = data.get("id")
+        package_name = data.get("items")[0]["price"]["name"]
+
+        # Get the customer email from Paddle
+        customer_email = get_customer_email(data)
+
+        users_collection = db.get_collection('users')
+        await users_collection.find_one_and_update(
+            {"email": customer_email},
+            {"$set": {"subscription_status": True,
+                    "userType": package_name,
+                    "subscription_id": sub_id,
+                    }},
+            upsert=True,
+            return_document=ReturnDocument.AFTER
+        )
+    except Exception as e:
+        print(f"Error handling subscription created event: {e}")
+        raise HTTPException(status_code=500, detail=f"Error handling subscription created event: {e}")
+    
+async def handleSubscriptionCanceled(data: dict):
+    """
+    Handle the subscription cancelled event from Paddle.
+    This function should be called when a subscription is cancelled.
+
+    It should update the user's subscription status in the database.
+    """
+    try:
+        customer_email = get_customer_email(data)
+        users_collection = db.get_collection('users')
+
+        await users_collection.find_one_and_update(
+            {"email": customer_email},
+            {"$set": {"subscription_status": False,
+                    "userType": "Free",
+                    "subscription_id": '',
+                    }},
+        )
+        
+    except Exception as e:
+        print(f"Error handling subscription cancelled event: {e}")
+        raise HTTPException(status_code=500, detail=f"Error handling subscription cancelled event: {e}")
+
+
