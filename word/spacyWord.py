c9c9364f09f635a8c7d5645749dc9f72b6be234b
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-05 11:01:06 +0300

    refactor: move NLP model loading inside functions to improve encapsulation and response time

--- word/spacyWord.py
+++ word/spacyWord.py
@@ -1,7 +1,5 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-
 def get_word_pos(word: str, sentence:str) -> str:
 
     """
@@ -11,6 +9,8 @@ def get_word_pos(word: str, sentence:str) -> str:
 
         :return: The part of speech of the word
     """
+
+    nlp = spacy.load("en_core_web_sm")
 
     # Load the English NLP model
     # Process a sentence
@@ -28,6 +28,8 @@ def calculate_similarity_score(word1 : str, word2 : str) -> float:
         :param word2: The second word
         :return: The similarity between the two words
     """
+
+    nlp = spacy.load("en_core_web_sm")
 
     word1 = nlp(word1)
     word2 = nlp(word2)
