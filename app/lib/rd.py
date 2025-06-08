5f649c864e76e0ff98bfc14481bfdd9772f76d87
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-08 13:30:36 +0300

    feat: update Redis connection settings with new host, port, username, and password

--- app/lib/rd.py
+++ app/lib/rd.py
@@ -1,3 +1,13 @@
 import redis
+import os
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+host = os.getenv('REDIS_HOST')
+password = os.getenv('REDIS_PASSWORD')
+
+r = redis.Redis(
+    host=host,
+    port=15382,
+    decode_responses=True,
+    username="default",
+    password=password,
+)
